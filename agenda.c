077c88e7afaf5aee3b134208d0445e296487b341
gwmagna22 <gwmagnabosco@inf.edu.ufpel.br>
2018-09-03 14:49:39 -0300

    alteraçao da leitura de entrada

--- agenda.c
+++ agenda.c
@@ -4,6 +4,9 @@
 typedef struct P {
 	char nome[20];
 } pessoa;
+typedef struct{
+	int i;
+} variaveis;
 
 void insere();
 void exclui();
@@ -12,44 +15,43 @@ void mostra();
 
 int main () {
 
-	pessoa *pbuffer;
-	char *resposta;
-	int *op;
-	*resposta = 'S'; 
-	
-	pbuffer = (pessoa *) malloc(sizeof(pessoa));
-	
-	while (*resposta != 'N') {
-	
-		printf("[ 1 ] Inserir\n [ 2 ] Excluir\n [ 3 ] Buscar\n [ 4 ] Mostrar");
-		scanf("%d", op);
-		
-		if (*op == 1) {
+	void *pbuffer;
+    variaveis *op;
+
+
+	op = pbuffer;
+
+	pbuffer = malloc(sizeof(int));
+
+	while (op->i != 5) {
+
+		printf("[>1<] Inserir\n[>2<] Excluir\n[>3<] Buscar\n[>4<] Mostrar \n[>5<] Sair\n");
+		scanf("%d", op->i);
+
+		if (op->i == 1) {
 			insere();
-		} else if (*op == 2) {
+		} else if (op->i == 2) {
 			exclui();
-		} else if (*op == 3) {
+		} else if (op->i == 3) {
 			busca();
 		} else {
 			mostra();
-		}				
-	
-		scanf("%c", resposta);
+		}
 	};
 
 
 }
 void insere() {
-	
+
 }
 void exclui() {
-	
+
 }
 void busca() {
 
-}	
+}
 void mostra() {
-	
-}		
-	
-	
+
+}
+
+
